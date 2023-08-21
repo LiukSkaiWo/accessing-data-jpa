49f06aed998b66b0cbd7c67bf67a8140de162264
LiukSkaiWo <albergo.vargroup@gmail.com>
2023-08-21 17:56:22 +0200

    Added modified files except .idea/

---
+++ src/main/java/it/xa/example/Accessing/Data/JPA/CustomerRepository.java
@@ -0,0 +1,13 @@
+package it.xa.example.Accessing.Data.JPA;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository <Customer, Long> {
+
+    List<Customer> findByLastName (String lastName);
+
+    Customer findById(long id);
+
+}
