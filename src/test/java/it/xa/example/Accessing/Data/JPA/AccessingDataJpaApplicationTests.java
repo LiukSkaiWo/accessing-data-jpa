d597d18057c3ba1d36a0bbbb188d69ada41491c5
LiukSkaiWo <albergo.vargroup@gmail.com>
2023-08-21 17:23:23 +0200

    Added first files without any modification

---
+++ src/test/java/it/xa/example/Accessing/Data/JPA/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.xa.example.Accessing.Data.JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
